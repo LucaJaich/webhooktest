a85196ff3eb622a39b895865a65a9e0312f5201d
LucaJaich <jaichencol@gmail.com>
2020-10-08 01:54:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '1086795',
+  key: 'cf58f89a3bd4a236a5c9',
+  secret: 'e52ce0397dde58b164c9',
+  cluster: 'us2',
+});
+
+exports.handler = async (event) => {
+    pusher.trigger('my-channel', 'my-event', {
+        'message': 'hello world'
+    });
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
