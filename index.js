9200f4479729c5531e9437927c86d292e1328ede
LucaJaich <jaichencol@gmail.com>
2020-10-08 03:09:36 -0300

    push

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var pusher = new Pusher({
   cluster: 'us2',
 });
 
-console.log('testo')
+console.log('tu mama')
 
 exports.handler = async (event) => {
     pusher.trigger('my-channel', 'my-event', {
